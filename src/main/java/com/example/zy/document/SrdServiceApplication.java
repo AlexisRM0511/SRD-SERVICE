b37536d40840c8c6da409840b7da42c134699008
Alexis Rojas <arojas@zytrust.com>
2023-03-08 15:48:57 -0500

    Jar Final 2

--- src/main/java/com/example/zy/document/SrdServiceApplication.java
+++ src/main/java/com/example/zy/document/SrdServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.zy.document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,25 +14,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMongoRepositories
 public class SrdServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SrdServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SrdServiceApplication.class, args);
+    }
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
+    @Configuration
+    public static class CrossOriginConfig {
 
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200", "https://srd-portal-dev.netlify.app")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
-			}
-		};
-	}
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry
+                            .addMapping("/**")
+                            .allowedOrigins("http://localhost:4200", "https://srd-portal-dev.netlify.app")
+                            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+                }
+            };
+        }
+
+    }
 }
+
